0314fb5726d6851eb64fe164fabcfb43e7dac8dd
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-01 00:27:33 +0300

    move_forward_weeks

--- move_date/util.py
+++ move_date/util.py
@@ -15,11 +15,15 @@ def move_forward(dt, step):
     if size == "day":
         return move_forward_days(dt, n)
     elif size == "week":
-        pass
+        return move_forward_weeks(dt, n)
     elif size == "month":
         pass
     elif size == "year":
         pass
+
+
+def move_forward_weeks(dt, nweeks):
+    return move_forward_days(dt, nweeks * 7)
 
 
 def move_forward_days(dt, ndays):
