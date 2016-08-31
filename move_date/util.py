409091b9d57df2e8971dd566097b6946e69fd575
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-01 00:26:05 +0300

    move_forward_days

--- move_date/util.py
+++ move_date/util.py
@@ -3,8 +3,28 @@ Utility module
 """
 
 
+import datetime
+
+
 def date2():
     return 'stub2'
+
+
+def move_forward(dt, step):
+    (n, size) = step
+    if size == "day":
+        return move_forward_days(dt, n)
+    elif size == "week":
+        pass
+    elif size == "month":
+        pass
+    elif size == "year":
+        pass
+
+
+def move_forward_days(dt, ndays):
+    delta = datetime.timedelta(ndays)
+    return dt + delta
 
 
 if __name__ == "__main__":
