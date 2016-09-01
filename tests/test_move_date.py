e29eed969b15f21d03b82d4b28ad80da7483dd73
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-01 10:03:15 +0300

    add tests for months

--- tests/test_move_date.py
+++ tests/test_move_date.py
@@ -35,6 +35,18 @@ class TestStringMethods(unittest.TestCase):
         exp = datetime.datetime(2016, 5, 31, 22, 55, 58)
         self.assertEqual(exp, d2)
 
+    def test_move_forward_months3(self):
+        d1 = datetime.datetime(2016, 3, 13, 22, 55, 58)
+        d2 = util.move_forward_months(d1, 3)
+        exp = datetime.datetime(2016, 6, 13, 22, 55, 58)
+        self.assertEqual(exp, d2)
+
+    def test_move_forward_months4(self):
+        d1 = datetime.datetime(2016, 3, 13, 22, 55, 58)
+        d2 = util.move_forward_months(d1, 2)
+        exp = datetime.datetime(2016, 5, 13, 22, 55, 58)
+        self.assertEqual(exp, d2)
+
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
